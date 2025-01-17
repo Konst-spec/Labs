90c20c52ceebd2e46a19d7df3eed3c907acec85d
Bolotbov Konstantin <bolotnovkonstantin@gmail.com>
2025-01-17 10:28:12 +0500

    Первый коммит

---
+++ Strings.py
@@ -0,0 +1,22 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.lower())
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
